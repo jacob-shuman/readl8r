d3351c94f6422811e410846a616c103d6dc9ad6b
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-09-03 10:06:10 -0400

    fix: atom route content type

--- src/routes/atom/+server.ts
+++ src/routes/atom/+server.ts
@@ -1,10 +1,10 @@
 import { generateFeed, getArticles } from '$lib/feed';
 import { type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ request }) => {
+export const GET: RequestHandler = async () => {
 	return new Response(generateFeed(getArticles()).atom1(), {
 		headers: {
-			'Content-Type': 'application/rss+xml'
+			'Content-Type': 'application/atom+xml'
 		},
 		status: 200
 	});
