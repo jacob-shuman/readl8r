6028934afa6694aeec2f405c69d00d94e4337d48
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-09-16 11:13:11 -0400

    chore: add todo tests for db functions

---
+++ src/lib/db.test.ts
@@ -0,0 +1,11 @@
+import { describe } from 'vitest';
+
+describe.todo('getDb()', () => {});
+describe.todo('addArticle()', () => {});
+describe.todo('getArticle()', () => {});
+describe.todo('getArticles()', () => {});
+describe.todo('updateArticle()', () => {});
+describe.todo('deleteArticle()', () => {});
+describe.todo('deleteAllArticles()', () => {});
+describe.todo('getPurgeDate()', () => {});
+describe.todo('purgeArticles()', () => {});
