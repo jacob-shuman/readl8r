7fc0943ad888c4de797ee8cd89d1b76049d355ae
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2024-10-02 11:34:07 -0400

    feat: loading and onclick props for button component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,30 +1,43 @@
 <script lang="ts">
+	import Icon from '@iconify/svelte';
 	import type { Snippet } from 'svelte';
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 	import { twMerge } from 'tailwind-merge';
 
 	let {
+		loading,
 		type,
 		href,
+		onclick,
 		children,
 		class: classes
 	}: {
 		children: Snippet;
+		onclick?: HTMLButtonAttributes['onclick'];
+		loading?: boolean;
 		type?: HTMLButtonAttributes['type'];
 		href?: string;
 		class?: string;
 	} = $props();
 
 	const buttonClasses =
-		'text-center w-full border border-dashed border-gray-light py-3 font-bold transition duration-100 ease-out hover:border-solid hover:underline focus:border-solid focus:border-white focus:underline focus:outline-none motion-safe:transition-all';
+		'text-center w-full border border-dashed border-gray-light py-3 font-bold duration-100 ease-out hover:border-solid hover:underline focus:border-solid focus:border-blacker focus:dark:border-white focus:underline focus:outline-none motion-safe:transition-all';
 </script>
+
+{#snippet loadingChildren()}
+	{#if loading}
+		<Icon icon="svg-spinners:clock" class="w-full" />
+	{:else}
+		{@render children()}
+	{/if}
+{/snippet}
 
 {#if href}
 	<a {href} class={twMerge(buttonClasses, classes)}>
-		{@render children()}
+		{@render loadingChildren()}
 	</a>
 {:else}
-	<button {type} class={twMerge(buttonClasses, classes)}>
-		{@render children()}
+	<button {type} {onclick} class={twMerge(buttonClasses, classes)}>
+		{@render loadingChildren()}
 	</button>
 {/if}
